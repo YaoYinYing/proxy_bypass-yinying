e6cbac0e41cf69c078d44656223f2a3bc072bc3e
yaoyy.hi@gmail.com <33014714+YaoYinYing@users.noreply.github.com>
2023-01-28 18:05:25 +0800

    apply the table to macos settings.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # proxy_bypass-yinying
 A list of domain/IP that will not use proxy.
+
+
+## Usage
+
+### Update IP/domain list
+Append ip/address to the `ip` column `data/proxy_bypass-yinying.csv`.
+
+### Apply the proxy bypassing
+
+
+#### MacOS
+```shell
+sh <repo>/scripts/apply_bypass_macos.sh
+
+```
---
+++ scripts/apply_bypass_macos.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# make it stop if error occurs
+set -e 
+batch_size=10
+
+run_dir="$(readlink -f $(dirname $0))"
+
+echo "$run_dir"
+
+# Get the active network interface
+active_interface=$(route -n get default | grep interface | awk '{print $2}')
+echo "Active interface device: $active_interface"
+
+# Get the list of all hardware ports and get the active network interface device ID
+active_interface_name=$(networksetup -listallhardwareports | sed -n '1!G;h;$p' |awk '{ret = match($0, "Device: '$active_interface'");if(ret>0){getline;print $3}}')
+echo "$active_interface_name is in use."
+
+# Read the IP addresses or domains from the 'bypass.csv' file
+IFS=','
+bypass_list=""
+
+echo "Open CSV file: $(readlink -f $run_dir/../data/proxy_bypass-yinying.csv)"
+while read -r ip _
+do
+    #echo "bypassing $ip to $active_interface_name"
+    bypass_list="$bypass_list $ip"
+    if [[ "$(echo $bypass_list | wc -w)" -ge "$batch_size" ]]; then
+      echo "Bypassing proxy for $bypass_list"
+      networksetup -setbypassdomains "$active_interface_name" "$bypass_list"
+      bypass_list=""
+    fi
+done < $run_dir/../data/proxy_bypass-yinying.csv
+
+# Set the proxy bypass settings for remaining IP addresses or domains
+if [[ -n $bypass_list ]]; then
+  echo "Bypassing proxy for $bypass_list"
+  networksetup -setbypassdomains "$active_interface_name" "$bypass_list"
+fi
+
+echo "Proxy bypass settings updated"
